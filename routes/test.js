8eeb738d133789467e29ca334310f2dbfa011f81
Hectorlobo10000 <hector.lobo10000@gmail.com>
2018-06-05 00:07:36 -0600

    Testing

--- routes/test.js
+++ routes/test.js
@@ -1,27 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var db = require('./firebase')
-
+var data;
 
 router.get('/api/test', (req, res) => {
 	var testRef = db.collection('Test').doc('ChapterTest');
-	var data = 'Hola';
 	var getDoc = testRef.get()
 		.then(doc => {
 			if (!doc.exists) {
-				console.log('Document no such;')
+				res.statusCode = 400;
+				res.send({
+					message: 'Document no such'
+				})
 			} else {
-				console.log('Document data: ', doc.data());
-				data = doc.data();
+				res.statusCode = 200;
+				res.send(doc.data());
 			}
 		})
 		.catch(err => {
 			console.log('Error getting document:', err);
 		});
-		res.send({
-			message: 'Sucess',
-			data
-		})
 });
 
 module.exports = router
